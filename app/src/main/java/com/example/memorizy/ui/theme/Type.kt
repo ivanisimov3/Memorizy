fc9567c3d02415e6c9a0383f939e4cc5692e10d0
ivanisimov <ivanisimov03@gmail.com>
2025-11-19 00:20:51 +0300

    new theme, colors, typo, icon

--- app/src/main/java/com/example/memorizy/ui/theme/Type.kt
+++ app/src/main/java/com/example/memorizy/ui/theme/Type.kt
@@ -2,33 +2,41 @@ package com.example.memorizy.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.memorizy.R
+
+val Commissioner = FontFamily(
+    Font(R.font.commissioner_regular)
+)
+
+val TikTokSans = FontFamily(
+    Font(R.font.tiktoksans_regular),
+    Font(R.font.tiktoksans_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = Commissioner,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = TikTokSans,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontSize = 20.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = TikTokSans,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = TikTokSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
     )
-    */
 )
