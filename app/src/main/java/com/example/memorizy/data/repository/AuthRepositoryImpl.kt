fc47cb2a6827b5f11f45a6f5ba690cbc746c1b16
ivanisimov <ivanisimov03@gmail.com>
2025-12-07 13:13:48 +0300

    AuthRepositoryImpl.kt code optimization

--- app/src/main/java/com/example/memorizy/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/memorizy/data/repository/AuthRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.example.memorizy.data.source.local.datastore.SettingsDataStore
 import com.example.memorizy.data.source.local.room.dao.StudySetDao
 import com.example.memorizy.data.source.network.MemorizyApiService
 import com.example.memorizy.data.source.network.dto.AuthRequest
+import com.example.memorizy.data.source.network.dto.AuthResponse
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.first
 
@@ -15,39 +16,26 @@ class AuthRepositoryImpl @Inject constructor(   // Inject coonstructor связ�
 ) : AuthRepository {
 
     override suspend fun register(request: AuthRequest): Result<Unit> {
-        return try {
-            val response = api.register(request)
-            val newUserId = response.userId
-
-            val lastUserId = settingsDataStore.userId.first()
-
-            // Если уже кто то был залогинен и это не он сейчас входит
-            if (lastUserId != newUserId){
-                studySetDao.clearSyncedData()  // Очищаем все наборы, связанные с другим аккаунтом
-            }
-
-            settingsDataStore.saveUserUtilInfo(response.token, newUserId)
-            settingsDataStore.saveUsername(request.username)
-
-            Result.success(Unit)
-        } catch (e: Exception) {
-            e.printStackTrace()
-
-            Result.failure(e)
-        }
+        return authAction(request) { api.register(it) }
     }
 
     override suspend fun login(request: AuthRequest): Result<Unit> {
+        return authAction(request) { api.login(it) }
+    }
+
+    private suspend fun authAction(
+        request: AuthRequest,
+        apiCall: suspend (AuthRequest) -> AuthResponse
+    ): Result<Unit> {
         return try {
-            val response = api.login(request)
+            val response = apiCall(request)
 
             val newUserId = response.userId
-
             val lastUserId = settingsDataStore.userId.first()
 
             // Если уже кто то был залогинен и это не он сейчас входит
-            if (lastUserId != newUserId){
-                studySetDao.clearSyncedData()  // Очищаем все наборы, связанные с другим аккаунтом
+            if (lastUserId != newUserId) {
+                studySetDao.clearSyncedData()   // Очищаем все наборы, связанные с другим аккаунтом
             }
 
             settingsDataStore.saveUserUtilInfo(response.token, newUserId)
