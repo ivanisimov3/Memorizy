75d016af7c8f8a86efd05f8ec37a7b6787f1b43b
ivanisimov <ivanisimov03@gmail.com>
2026-03-18 16:29:03 +0300

    new app font

--- app/src/main/java/com/example/memorizy/ui/theme/Type.kt
+++ app/src/main/java/com/example/memorizy/ui/theme/Type.kt
@@ -4,51 +4,47 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.memorizy.R
 
-val InterBold = FontFamily(
-    Font(R.font.inter_bold)
-)
-
-val InterItalic = FontFamily(
-    Font(R.font.inter_italic)
-)
-
-val InterRegular = FontFamily(
-    Font(R.font.inter_regular)
+val MarmeladRegular = FontFamily(
+    Font(R.font.marmelad_regular)
 )
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = InterBold,
+        fontFamily = MarmeladRegular,
         fontSize = 28.sp
     ),
     displayMedium = TextStyle(
-        fontFamily = InterBold,
+        fontFamily = MarmeladRegular,
         fontWeight = FontWeight.Bold,
         fontSize = 24.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = InterRegular,
+        fontFamily = MarmeladRegular,
         fontSize = 24.sp
     ),
     bodySmall = TextStyle(
-        fontFamily = InterRegular,
+        fontFamily = MarmeladRegular,
         fontSize = 18.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = InterItalic,
-        fontSize = 28.sp
+        fontFamily = MarmeladRegular,
+        fontSize = 28.sp,
+        fontStyle = FontStyle.Italic
     ),
     labelMedium = TextStyle(
-        fontFamily = InterItalic,
-        fontSize = 24.sp
+        fontFamily = MarmeladRegular,
+        fontSize = 24.sp,
+        fontStyle = FontStyle.Italic
     ),
     labelSmall = TextStyle(
-        fontFamily = InterItalic,
-        fontSize = 18.sp
+        fontFamily = MarmeladRegular,
+        fontSize = 18.sp,
+        fontStyle = FontStyle.Italic
     )
 )
