1ea7818302ab7f0bb6b82a95c5786fc6bef9e434
ivanisimov <ivanisimov03@gmail.com>
2026-05-01 20:21:00 +0300

    app version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
         buildConfigField("String", "BASE_URL", "\"$memorizyBaseUrl\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
